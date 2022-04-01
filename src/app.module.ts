3c1494d896224d6091281cd8144e014d5538a749
hoangbui <hoang21099@gmail.com>
2022-04-01 10:57:06 +0700

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
+import { Transaction } from './transactions/transactions.entity';
+
 import { Connection } from 'typeorm';
 import { TransactionsController } from './transactions/transactions.controller';
 import { TransactionsModule } from './transactions/transactions.module';
@@ -20,7 +22,10 @@ import { TransactionsModule } from './transactions/transactions.module';
       type: 'mongodb',
       url: process.env.MONGO_URI,
       database: process.env.MONGO_DB,
-      entities: [User],
+      //process.env.MONGO_DB,
+      entities: [User, Transaction],
+      username: process.env.MONGO_USERNAME,
+      password: process.env.MONGO_PASSWORD,
       // ssl: true,
       // useUnifiedTopology: true,
       // useNewUrlParser: true,
@@ -34,6 +39,10 @@ import { TransactionsModule } from './transactions/transactions.module';
 })
 export class AppModule {
   constructor(private connection: Connection) {
-    console.log('xasasas');
+    console.log('\n\n\n\n\nxasasas');
+    console.log('process.env.MONGO_DB: ', process.env.MONGO_DB);
+
+    console.log('process.env.MONGO_USERNAME: ', process.env.MONGO_USERNAME);
+    console.log('process.env.MONGO_PASSWORD: ', process.env.MONGO_PASSWORD);
   }
 }
